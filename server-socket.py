5197646f8f711697273b67a6073d07222d0cab9f
Andrew Moore <asmoor@gmail.com>
2015-03-31 19:05:33 -0400

    add test comment

--- server-socket.py
+++ server-socket.py
@@ -33,18 +33,30 @@ def join(message):
 
 
 def server_backend():
-    for comment in praw.helpers.comment_stream(r, subreddit):
-        print comment.author.name
-        comment_dict = {"author": comment.author.name, 
-                         "body": comment.body, 
-                         "author_flair_css_class": comment.author_flair_css_class, 
-                         "comment_id": comment.id, 
-                         "score": comment.score,
-                         "created_utc": comment.created_utc, 
-                         "emitted": "false",
-                         "thread_id": (comment.link_id).split("_",1)[1]}
+    while True:
+        time.sleep(5)
+        comment_dict = {"author": "catmoon", 
+                        "body": "test", 
+                        "author_flair_css_class": "Heat1", 
+                        "comment_id": "qwresdf", 
+                        "score": "sdfsafd",
+                        "created_utc": "123123123", 
+                        "emitted": "true"}
         message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
         socketio.emit('echo', message, room='2zeobb')
+    
+    #for comment in praw.helpers.comment_stream(r, subreddit):
+    #    print comment.author.name
+    #    comment_dict = {"author": comment.author.name, 
+    #                     "body": comment.body, 
+    #                     "author_flair_css_class": comment.author_flair_css_class, 
+    #                     "comment_id": comment.id, 
+    #                     "score": comment.score,
+    #                     "created_utc": comment.created_utc, 
+    #                     "emitted": "false",
+    #                     "thread_id": (comment.link_id).split("_",1)[1]}
+    #    message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
+    #    socketio.emit('echo', message, room='2zeobb')
 
 
 if __name__ == '__main__':
