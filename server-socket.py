3d1103e7924b1176ce8a1085bcfd574ab7660f07
Andrew Moore <asmoor@gmail.com>
2015-03-31 19:27:15 -0400

    print thread

--- server-socket.py
+++ server-socket.py
@@ -69,5 +69,6 @@ if __name__ == '__main__':
     #if thread is None:
     #    print "start thread"
     thread = Thread(target=server_backend)
+    print thread
     thread.start()
     socketio.run(app)
